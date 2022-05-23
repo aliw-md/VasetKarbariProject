210ce4665e6cb10aa0f60e741984b45b8538f6f4
aliw-md <aliw.md1@gmail.com>
2022-05-23 13:15:02 +0430

    Movement estricted to parent.

--- src/drag.js
+++ src/drag.js
@@ -1,7 +1,17 @@
 const position = { x: 0, y: 0 }
+// target elements with the "draggable" class
 interact('.DraggableSun').draggable({
-    startAxis: 'x',
+    //startAxis: 'x',
     lockAxis: 'x',
+    // enable inertial throwing
+
+    inertia: true,
+    // keep the element within the area of it's parent
+    restrict: {
+        restriction: "parent",
+        endOnly: true,
+        elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    },
     listeners: {
         start(event) {
             console.log("I am Sun: ", event.type, event.target)
@@ -14,10 +24,17 @@ interact('.DraggableSun').draggable({
                 `translate(${position.x}px, ${position.y}px)`
         },
     }
+
 });
 interact('.DraggableCloud').draggable({
-    startAxis: 'x',
+    //startAxis: 'x',
     lockAxis: 'x',
+    inertia: true,
+    restrict: {
+        restriction: "parent",
+        endOnly: true,
+        elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    },
     listeners: {
         start(event) {
             console.log("I am cloud: ", event.type, event.target)
