b4115c29b040c4d0806e4de4fff22a032ac2d07d
aliw-md <aliw.md1@gmail.com>
2022-05-26 20:33:34 +0430

    Added overlap percentage function

--- src/functions.js
+++ src/functions.js
@@ -25,11 +25,28 @@ function IsElementsOverlapping(el1, el2) {
     const domRect2 = el2.getBoundingClientRect();
 
     return !(
-        domRect1.top > domRect2.bottom ||
         domRect1.right < domRect2.left ||
-        domRect1.bottom < domRect2.top ||
         domRect1.left > domRect2.right
     );
 }
+function ElementsOverlapPercentage(el1, el2) {
+    const domRect1 = el1.getBoundingClientRect();
+    const domRect2 = el2.getBoundingClientRect();
+    var El1Width = domRect1.right - domRect1.left;
+    var El2Width = domRect2.right - domRect2.left;
+
+    var Overlap;
+    var BiggerWidth = Math.max(El1Width, El2Width);
+    if (domRect1.left < domRect2.left) {
+        Overlap = Math.abs(domRect1.right - domRect2.left);
+    } else {
+        Overlap = Math.abs(domRect2.right - domRect1.left);
+    }
+    console.log("overlap: ", Overlap);
+    console.log("El1Width: ", El1Width, "El2Width: ", El2Width);
+
+    console.log(parseFloat(100 / (BiggerWidth / Overlap)));
+}
+
 
 
